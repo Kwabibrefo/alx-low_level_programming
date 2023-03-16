005bfcb2c323fee63a891b56d0f4e3e847bd3082
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-16 16:04:35 -0700

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 
-/*description: printing with printf*/
+/**
+ * main - Entry point
+ * Description: 'print using print f'
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
