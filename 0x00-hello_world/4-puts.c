9a03e8a7cefe5956786b84e45aec100ec36549a7
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-16 15:41:41 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-/*code to print a scentence using puts*/
-
 #include<stdio.h>
+
+/*code to print a scentence using puts*/
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n\"");
+puts("\"Programming is like building a multilingual puzzle\"\n");
 return (0);
 }
