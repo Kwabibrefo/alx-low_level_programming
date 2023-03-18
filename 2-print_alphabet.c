4533875112acc325702bbe016d3d9cd3aa722139
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-18 14:38:11 -0700

    ef

---
+++ 2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib>
+
+/**
+ * description: uyu
+ * return (0) success
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter; <='z'; letter++)
+		putchar(letter);
+	return (0)
+}
