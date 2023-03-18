475ea022350858f930544b5e5443227b7214dded
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-18 07:21:08 -0700

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, msg, strlen(msg));
+	fwrite(STDERR_FILENO, msg, strlen(msg));
 	return (0)
 }
