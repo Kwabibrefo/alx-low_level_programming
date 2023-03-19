f12322595dee43eb3b09a0462e92013aa243aec8
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-19 12:18:56 -0700

    ff

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main to enter function
- * Description: 'print alphs without q and e
- * Return: return 0 (success)
- * */
+ * main - entry point
+ * Description: printing upper and loer case alphabets
+ * Return: always return 0 (success)
+ */
 
 int main(void)
 {
