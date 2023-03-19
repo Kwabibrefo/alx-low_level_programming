702d1e0240f8d2adb5ad1cc96d8fef00dc46b0b3
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-19 03:18:39 -0700

    t

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main to enter function
+ * Description: print alphs without q and e
+ * Return: return 0 (success)
+ * */
+
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++ != q, e)
+		putchar(letters);
+	putchar('\n');
+	return (o);
+}
