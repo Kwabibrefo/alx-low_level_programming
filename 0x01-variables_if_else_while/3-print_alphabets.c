7ab096423fa827e4207477513759eb008196ae64
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-19 02:05:05 -0700

    t

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - entry poin
+ * Description: printing upper and loer case alphabets
+ * return: always return 0 (success)
+ */
+
+int main(void)
+{
+	char lo_case_letters
+	char up_case_letters
+
+	for (lo_case_letters = 'a', lo_case_letters <= 'z', lo_case_letters++)
+	for (up_case_letters = "A", up_case_lettrs <= 'Z', up_case_letters++)
+		putchar(lo_case_letters+up_case letters);
+	putchar('\n')
+	return (0);
+}
