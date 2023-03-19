d4f143d05b505314c41a527dc6632535cb722f5a
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-19 14:21:29 -0700

    h

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: code to print letters in base 10
+ * Return: return 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a + '0');
+	putchar('\n');
+	return (0);
+}
