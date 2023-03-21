884fa80276f1686788c112dcf11cb450450e3d2b
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-21 09:36:08 -0700

    d

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - entry point
+ * Description: code to print _putchar
+ * Return: return 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
