babd0bcf0963b399a6efcea5505c2e2affc498a2
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-21 16:04:35 -0700

    ff

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,23 @@
 #include  <stdio.h>
 
 /**
- * Main - entry point
+ * main - entry point
  * description: print comb of single digit numbers separated by comma and space
  * Return: return 0 (success)
  */
 
 int main(void)
 {
-	int i = 0;
+	int i = 48;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		putchar(i + '0');
-		if (i < '9')
+		putchar(i);
+		if (i < 57)
 		{
 			putchar(44);
 		}
-		if (i < '9')
+		if (i < 57)
 		{
 			putchar(32);
 		}
