fad95b6658d53cd5b3ebd39f9d5ad2f13558bea3
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-22 16:08:37 -0700

    g

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @n: int char
+ * Return: return 0 (success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+	return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+	return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+	return (-1)
+	}
+}
