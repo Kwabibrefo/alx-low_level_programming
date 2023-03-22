2d1033f8391a93b350852767c2045c4420c7acc1
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-22 16:45:32 -0700

    g

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * @n: last digit
+ * Return: return n (success)
+ */
+
+int print_last_digit(int n)
+{
+	_putchar(n % '10');
+return (n);
+}
