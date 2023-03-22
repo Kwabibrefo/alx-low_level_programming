0bb32344b24cfb5a54192ebca97b2c2900606d05
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-22 15:28:05 -0700

    g

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ * @c: letter fr int
+ * Return : return 0 (success)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
