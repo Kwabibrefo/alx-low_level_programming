81d92c99e11622d0a1f9d1ad7fdd4e4c19442993
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-21 19:23:48 -0700

    low alph

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - entry point
- * Description: alphabets print out
- * Return: return 0 (success)
- */
+ * * print_alphabet_x10 - entry point
+ * * Description: alphabets print out
+ * * Return: return 0 (success)
+ * */
 
 void print_alphabet_x10(void)
 {
-	int i = 97;
+	int i = 0;
 	int k = 0;
+
 	while (k <= 10)
 	{
-		for (i = 97; i <= 122; i++)
+		for (i = 0; i <= 10; i++)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 	_putchar('\n');
 	}
