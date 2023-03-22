63a0db4c280be4da4be7b2365dd711ce4c76e1e7
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-22 14:45:53 -0700

    g

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * Description : predict let cases
+ * Return: return 0,1 (success)
+ */
+
+int _islower(int c);
+{
+	int i = 65;
+
+	if (i >= 97; i < 122)
+	{
+		_putchar(1);
+	return (1);
+
+	}
+	return (0);
+}
