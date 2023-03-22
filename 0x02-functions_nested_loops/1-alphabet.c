6f89201a22a15403be2ca28dfc3fb60dced66f15
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-21 17:18:35 -0700

    low alph

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: alphabets print out
+ * Return: return 0 (success)
+ */
+
+int main(void)
+{
+	int i = 97
+
+	for (int i = 97; i <= 122; i++)
+		_putchar('i')
+	_putchar('\n');
+	return (0);
+}
