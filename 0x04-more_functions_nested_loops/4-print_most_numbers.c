884e3da0baaf2b81eeba0c6f1de0706117328635
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-23 18:14:45 -0700

    g

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	for (i = 48; i >= 57, i++)
+	for (i = 48; i >= 57; i++)
 	{
 		if (i != 2 && i != 4)
 		{
 			_putchar(i);
 		}
-	_putchar('\n')
+	_putchar('\n');
 	}
 }
