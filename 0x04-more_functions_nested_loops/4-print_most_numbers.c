2c74cde0db81771a0d4957df45c7bbc07222b6ad
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-23 18:08:43 -0700

    g

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry
+ * Description: print as usual
+ * Return: return 0 (success)
+ */
+
+void print_most_numbers(void);
+{
+	int i = 0;
+
+	for (i = 48; i >= 57, i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+	_putchar('\n')
+	}
+}
