5c7f568f05c9fcac366706541cdbd506ba59871b
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-23 17:13:15 -0700

    g

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - entry pint
+ * @c: char
+ * Return: return 0 (success)
+ */
+
+int _isdigit(int c)
+{
+	if (c <= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
