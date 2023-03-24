9cc4ab74a0802e75ceb3c6f1ecf98bf1e8874375
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-23 17:47:20 -0700

    g

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - ent
+ * Description: to print numb
+ * Return: return 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int i = 0
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
