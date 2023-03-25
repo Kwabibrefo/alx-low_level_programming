c206a79950d8f82d7ad458580651ebc4523170dd
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-24 17:33:40 -0700

    fixing code to output for leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,17 +14,17 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-		if (month == 2 && day <= 60)
+		if (month == 2 && day == 60)
 		{
-			printf("Day ofthe year: %d\n", day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
 	}
-	else if (month == 2 && day >=60)
+	else if (month == 2 && day == 60)
 	{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
-	else if (day >=1 && day <=365)
+	else
 	{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
