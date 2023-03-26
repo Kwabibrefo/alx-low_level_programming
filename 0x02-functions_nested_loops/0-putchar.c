15e1df0a1cbf6df272c54b311291006e4a3f86a6
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-26 16:58:18 -0700

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * Main - entry point
@@ -8,7 +8,14 @@
 
 int main(void)
 {
-	char p = "_putchar"
-		
-	putchar('p');
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+	return (0);
 }
