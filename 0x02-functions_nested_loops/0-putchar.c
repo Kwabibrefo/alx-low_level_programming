e99adf1d6131b4c011874f257f58ceeccbec3474
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-26 17:09:44 -0700

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,15 +3,12 @@
 /**
  * main - entry point
  * Description: print _putchar
- * @c: variable
  * Return: return 0 (success)
  */
 
 int main(void)
 {
-	int c;
-
-	c = 0;
+	
 	_putchar(95);
 	_putchar(112);
 	_putchar(117);
@@ -21,5 +18,5 @@ int main(void)
 	_putchar(97);
 	_putchar(114);
 	_putchar('\n');
-	return (c);
+	return (0);
 }
