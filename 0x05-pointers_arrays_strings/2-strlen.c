1f47836c8f9ebe471d890e298ccf39f08afa90d1
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-28 16:52:35 -0700

    counting length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _strlen - entry point
  * @s: some pointer
- * Return value: return nothing
+ * Return: return length (succcess)
  */
 
 int _strlen(char *s)
 {
+	int i;
+	int l = 0;
 
-	printf("%lu", sizeof(*s));
-	return (0);
+	for (i = 0; s[i] != 0; i++)
+	{
+		l++;
+	}
+	return (l++);
 }
