d6bf6b74020c9ce40805191c484f0021c70bfa00
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-29 15:33:06 -0700

    printing every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,16 @@
 void puts2(char *str)
 {
 	int i;
-
-	for (i = 0; str[i] != '\0'; i = i + 2)
+	int c;
+	int d = 0;
+	int e;
+	for (i = 0; str[i] != '\0'; i++ )
 	{
-		_putchar(str[i]);
+		e = d++;
+	}
+	for (c = 0; str[c] <= str[e - 1]; c = c + 2)
+	{
+		_putchar(str[c]);
 	}
 	_putchar('\n');
 }
