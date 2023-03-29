c29ab3312200a1ab15b972d18b95a20ca3a39a21
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-29 15:21:29 -0700

    printing every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - entry
+ * @str: some variable
+ * Return: ret none
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
