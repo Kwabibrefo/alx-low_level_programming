d0610147af5ad8962f8d4f53b256311094fc1796
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-30 06:38:30 -0700

    updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,18 +8,14 @@
 
 void puts2(char *str)
 {
-	int i;
-	int c;
-	int d = 0;
+	int i, j = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
+		;
+	while (j <= i)
 	{
-		c = i++;
-	}
-	while (d <= c)
-	{
-		_putchar(str[d]);
-		d = d + 2;
+		_putchar(str[j]);
+		j += 2;
 	}
 
 	_putchar('\n');
