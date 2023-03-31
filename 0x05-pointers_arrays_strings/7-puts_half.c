0d3e3899a587edbf8f8f6c60b243773bf66459ee
kwabibrefo <kbnbrefonimo@gmail.com>
2023-03-31 03:15:59 -0700

    printing last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half -  entry point
+ * @str: variable
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j = 0;
+	int n;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		j++;
+	}
+	n = i / 2;
+	for (i = n; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+}
