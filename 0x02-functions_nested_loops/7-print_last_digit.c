422dfb142db7688422c817092ae25b9a8e420ad1
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-01 08:49:24 -0700

    correcting

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,16 @@
 int print_last_digit(int n)
 {
 	int x;
-	x = (n % 10);
-	_putchar(x + '0');
+
+	if (n > 0)
+	{
+		x = (n % 10);
+		_putchar(x + '0');
+	}
+	else
+	{
+		x = (n % 10) * (-1);
+                _putchar(x + '0');
+	}
 	return (x);
 }
