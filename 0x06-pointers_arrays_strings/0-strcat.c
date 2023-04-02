0efb3958cd533e1e4c1104959a9ec96ea953e6bf
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-02 09:11:29 -0700

    pushing all files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcat - entry point
+ * @*dest: char
+ * @*src: char
+ * Return: new dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{i
+		;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
