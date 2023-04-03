6316e86c22566896ff0bc2cab3b231e394d30bc6
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-03 11:04:25 -0700

    correcting to print n bytes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+
 	n = 0;
-	while (src != '\0')
+	while (src[n] != '\0')
 	{
 		dest[i] = src[n];
 		i++;
 		n++;
 	}
 	dest[i] = '\0';
+	return (dest);
 }
