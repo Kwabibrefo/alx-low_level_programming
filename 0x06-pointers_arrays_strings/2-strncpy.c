63ac461f8c7fe82f6c8c0e31a4882e5d96663edf
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-03 15:47:56 -0700

    correcting for null

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	n = 0;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	while (i < n)
+	for (; i < n; i++)
 	{
 		dest[i] = '\0';
 	}
 	return (dest);
 }
+
