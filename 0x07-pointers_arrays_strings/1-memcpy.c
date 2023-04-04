4f4924fc126c2ee384f7dc4ce1c9646e54c5423c
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-04 03:33:49 -0700

    çode to copy memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,15 +11,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	unsigned int l;
-
-	for (l = 0; dest[l] != '\0'; l++)
-	{
-		;
-	}
+	
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[l + i] = src[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
