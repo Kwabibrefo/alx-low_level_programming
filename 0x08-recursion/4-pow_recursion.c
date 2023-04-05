5c019c228e02272c80ef4ab9a7e99cf060e1316d
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-05 15:12:19 -0700

    power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion -recursion fun
+ * @x: int
+ * @y: power of int
+ * Return: int to the power of int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y = 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
