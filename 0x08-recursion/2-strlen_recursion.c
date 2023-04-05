7f516573dd16800c61b1872115e19568eaffc844
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-05 14:45:46 -0700

    length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,10 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		return (1 + _strlen_recursion(s + 1));
+		
+		return (0);
 	}
-	else
-	{
-		return ('\n');
-	}
+	return (1 + _strlen_recursion(s + 1));
 }
