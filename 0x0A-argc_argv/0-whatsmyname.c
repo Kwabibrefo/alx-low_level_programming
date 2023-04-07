6ed56a63f42f34b6c38d6beb9d40ee3fadf19ea1
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-07 09:46:58 -0700

    print file name no matter what

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,8 @@
  * Return: return zero (success)
  */
 
-int main(int argc, char **argv)
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	(void) argc;
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
