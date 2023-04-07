976ac5618907903d33209835ed2ae911cfcd8784
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-07 09:33:45 -0700

    print file name no matter what

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * maint - entry
+ * Description: prog to print file name
+ * Return: return 0 (success)
+ */
+
+int main (int argc, char **argv)
+{
+	(void) argc;
+		printf ("%s\n",argv[0]);
+	return (0);
+}
