6bec7d5e0b4fe05d6b61a6121bb2c03dd8c6ae59
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-12 15:04:26 -0700

    2 dimensional array size

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,15 +30,18 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < width; i++)
 	{
 		grid[i] = malloc(height * sizeof(int));
-		if (grid[i] == NULL)
-		{
-			return (NULL);
-			free(grid[i]);
-		}
-		free(grid);
 		for (j = 0; j < height; j++)
 		{
-			grid[i][j] = 0;
+			if (grid[i] == NULL)
+			{
+				return (NULL);
+				free(grid[i]);
+			}
+
+			else
+			{
+				grid[i][j] = 0;
+			}
 		}
 	}
 	return (grid);
