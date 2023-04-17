37a60368ac5d6a757c38a5307228d9a35bc8053a
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-17 02:20:55 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * struct dog - struct for data gathering on dogs
+ * @name: name of dog
+ * @owner: name of owner
+ * @age: age of dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float *age;
+}
