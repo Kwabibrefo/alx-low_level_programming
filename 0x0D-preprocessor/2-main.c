a48eb6b71f605670dbd2c281d2b6363ae967d399
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-17 13:36:56 -0700

    add function-like macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: returns the name of the file
+ * Return: nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
