7e85d7f72377f7382a5bab10a34cbf66d9190e42
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-17 08:12:26 -0700

    printing dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,12 @@ struct dog
 	float age;
 };
 
+/**
+ * dog_t - typdef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
