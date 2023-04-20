0ba4f032664c6a842aaff0362978d55eab8c42fd
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-20 14:30:57 -0700

    add operand codes

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_op_func - function pinter
+ * @s: pointer variable
+ * Return: zero
+ */
+
+ int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div }
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+	int o = 0;
+	
+	while (o < 5)
+	{
+		if (strcmp(s, ops[o].op) == 0)
+			return (ops[o].f);	
+		o++;
+	}
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - ddition function
+ * @a: one variabke
+ * @b: second variable
+ *
+ * Return: operation result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction function
+ * @a: a variable
+ * @b: another variabke
+ * Return: subtraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication function
+ * @a: variable one
+ * @b:variable two
+ *  Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: variable one
+ * @b:variable two 
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus function
+ * @a: variable
+ * @b: another variable
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
