be33969e926c50d9031ff5f9b4d8e1376919c7fa
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-29 16:12:41 -0700

    print unique combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - entry poit
+ * Description: print combs
+ * Return: return 0 (succcess)
+ */
+
+int main(void)
+{
+	int i = 48;
+	int j;
+	int k;
+
+	while (i <= 57)
+	{
+		for (j = 48; j <= 57 ; j++)
+		{
+			if (j > i)
+			{
+				for (k = 48; k <= 57; k++)
+				{
+					if (k > j)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(k);
+						if (i != '7' || j != '8' || k != '9')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
