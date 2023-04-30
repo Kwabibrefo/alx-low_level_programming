04604380a658cc0a0a42da16033d9438b28d7108
kwabibrefo <kbnbrefonimo@gmail.com>
2023-04-30 00:55:10 -0700

    complete task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - entry poit
+ * main - entry point
  * Description: print combs
  * Return: return 0 (succcess)
  */
@@ -10,18 +10,19 @@ int main(void)
 {
 	int i = 0;
 	int j = 0;
-	for(i = 0; i < 100; i++)
-    	{
-		for(j = 0; j < 100; j++)
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
 		{
-			if(i <= j)
+			if (i <= j)
 			{
 				putchar(i / 10 + '0');
 				putchar(i % 10 + '0');
 				putchar(' ');
 				putchar(j / 10 + '0');
-			       	putchar(j % 10 + '0');
-				if(i != 99 || j != 99)
+				putchar(j % 10 + '0');
+				if (i != 99 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
