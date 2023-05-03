b22df13e80bce79f3e5e330486ec8a304cf19be7
kwabibrefo <kbnbrefonimo@gmail.com>
2023-05-02 17:57:33 -0700

    push code

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - ...
+ * @head: pointer to list
+ * Return node number
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t counter = 0;
+	listint_t *current, *next;
+
+	if (h == NULL || *h == NULL)
+	{
+		return (0);
+	
+	current = *h;
+	}
+	
+	while (current != NULL)
+	{
+	       next = current->next;
+	       free(current);
+	       counter++;
+	
+	if (current <= next)
+	{
+		*h = NULL;
+		break;
+	}
+	current = next;
+	}
+
+	return (counter);
+
+
+}
