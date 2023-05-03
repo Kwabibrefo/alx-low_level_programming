05216f0e0054aa2f50502007e7f602ae1d1240a3
kwabibrefo <kbnbrefonimo@gmail.com>
2023-05-02 18:25:52 -0700

    push final code

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <main.h>
+
+/**
+ * find_listint_loop - finds the first node in list
+ * @head: pointer to the head node
+ * Return: node address  
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slo = head, *fast = head;
+	
+	while (fast != NULL && fast->next != NULL)
+	{
+		slo = slo->next;
+		fast = fast->next->next;
+		if (slo == fast)
+		{
+			while (head != slo)
+			{
+				head = head->next;
+				slo = slo->next;
+            }
+            return head;
+        }
+    }
+
+    return NULL;
+
