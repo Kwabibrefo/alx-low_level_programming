15e3a58d90ddd4b116afdccf61172a93404d243b
kwabibrefo <kbnbrefonimo@gmail.com>
2023-05-18 14:50:23 -0700

    .try something

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints string & new line at the end
+ * @separator: string
+ * @n: number of elements
+ * Return: void
+ */
+
+void print_all(const char * const format, ...);
+{
+	unsigned int i = 0;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (format == s)
+		{
+			char *p;
+			
+			p = (va_arg(ap, char *));
+		}
+		else if (format == i)
+		{
+			int *p;
+			p = (va_arg(ap, int *));
+		}
+			if (p != NULL)
+			{
+				printf("%s", p);
+				if (separator != NULL && i != (n - 1))
+				{
+					printf("%s", separator);
+				}
+			}
+			else
+			{
+				printf("(nil)");
+			}
+	}
+	putchar('\n');
+	va_end(ap);
+}
