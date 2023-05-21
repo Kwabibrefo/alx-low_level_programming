a04e70b423fa2d1b62a5f11af41630070d2a2bcc
kwabibrefo <kbnbrefonimo@gmail.com>
2023-05-21 15:26:15 -0700

    correct betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,7 +12,7 @@ void print_binary(unsigned long int n)
 	int i, count = 0;
 	unsigned long int current;
 	int num_bits = sizeof(unsigned long int) * CHAR_BIT;
-	
+
 	for (i = num_bits - 1; i >= 0; i--)
 	{
 		current = n >> i;
