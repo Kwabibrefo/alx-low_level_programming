0e75c62cf829ae0a9b930eb5b2ac2d0097f8babe
kwabibrefo <kbnbrefonimo@gmail.com>
2023-07-10 16:51:15 -0700

    output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads file and print to stdo
+ * @filename: name of read file
+ * @leteers: number of letters to print
+ * Return: 0 if fails or number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f, x, y;
+	char *par;
+
+	if (!filename)
+		return (0);
+	f = open(filename, o_RDONLY);
+	if (f < 0)
+		return (0);
+	par = malloc(sizeof(char) * letters);
+	if (!par)
+		return (0);
+	x = read(f, par, letters);
+	if (x < 0)
+	{
+		free (par);
+		return (0);
+	}
+		par[i] = '\0';
+	close(f);
+	y = write(STDOUT_FILENO, par, x);
+	if (y < 0)
+	{
+		free(par);
+		return (0);
+	}
+	free(par);
+	return (y);
+}
