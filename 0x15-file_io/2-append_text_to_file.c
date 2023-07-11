1cef78691844541a86a48ad37bfccbffd6f1dbc7
kwabibrefo <kbnbrefonimo@gmail.com>
2023-07-10 17:09:09 -0700

    append to file end

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at file end
+ * @filename: file 
+ * @text_content: new append
+ *
+ * Return: 1 on success and -1 if failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int f x, y = 0;
+	if (!filename)
+		return (-1);
+	f = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[b])
+			y++;
+		a = write(fd, text_content, b);
+		if (x != y)
+			return (-1);
+	}
+	close(f);
+	return (1);
+}
